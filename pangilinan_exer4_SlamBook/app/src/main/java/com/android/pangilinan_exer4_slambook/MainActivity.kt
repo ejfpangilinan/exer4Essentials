d7c5151e838028bad5e6d2b0e2f4c11bde0bb20c
Erl Jhon <efpangilinan1@up.edu.ph>
2020-03-03 18:58:44 +0800

    Exer 4 essentials

---
+++ pangilinan_exer4_SlamBook/app/src/main/java/com/android/pangilinan_exer4_slambook/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.android.pangilinan_exer4_slambook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
